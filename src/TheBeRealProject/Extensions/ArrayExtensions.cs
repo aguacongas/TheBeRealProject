4a2e69291dba7aedf03eb32ae0a7254db3aad78b
Olivier Lefebvre <aguacongasu@gmail.com>
2023-03-01 22:04:46 +0100

    refactor: random index at load

---
+++ src/TheBeRealProject/Extensions/ArrayExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System; 
+
+public static class ArrayExtensions 
+{
+    public static int GetBoundIndex(this Array array, int from) 
+    {
+        if (array is null || array.Length == 0) {
+            return 0;
+        }
+        if (from < array.Length) {
+            return from;
+        }
+        return array.GetBoundIndex(from - array.Length);
+    }
+}
